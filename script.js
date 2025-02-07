9cfcc4e3ecb782e57805e73827c9602e8bc9f315
raihanguntur <53545798+raihanguntur@users.noreply.github.com>
2025-02-07 23:32:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.getElementById('surpriseButton').addEventListener('click', function() {
     const surpriseMessage = document.getElementById('surpriseMessage');
+    const backgroundMusic = document.getElementById('backgroundMusic');
+
     surpriseMessage.classList.remove('hidden');
+    backgroundMusic.play(); // Memutar lagu
     this.style.display = 'none'; // Sembunyikan tombol setelah diklik
 });
